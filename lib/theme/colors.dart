7a27720b4ea774c8054ef983e4e20df07b43c3a0
Ebru Şara Bağca <ebru.sara123@outlook.com>
2023-01-17 19:53:43 +0300

    Necessary files have been arranged according to the new features

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -6,5 +6,5 @@ const Color black = Color(0xFF000000);
 const Color white = Color(0xFFFFFFFF);
 const Color color5 = Color(0xFFF6F6F6);
 const Color color6 = Color(0xffa1f480);
-const Color color7 = Color(0xffbdf2d5);
+const Color color7 = Color(0xffdc79d7);
 const Color color8 = Color(0xff3dbdc2);
